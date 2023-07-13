b343fbc142263bdf5aab57c63f83f217ace2bbd1
yuriossugui <138835284+yuriossugui@users.noreply.github.com>
2023-07-13 19:17:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function calcularInspecao() {
-  var inputPorcentagem = document.getElementById("inputPorcentagem").value;
-  var inputPecas = document.getElementById("inputPecas").value;
-  var inputCodigos = document.getElementById("inputCodigos").value;
-
-
-  var per = inputPorcentagem / 100;
-
-  var pecas = parseInt(inputPecas);
-  var codigos = parseInt(inputCodigos);
-
-  var resultadoPorCodigo = (pecas * per) / codigos;
-  var resultadoTotal = pecas * per;
-
- 
-  var resultadoElement = document.getElementById("resultadoPorCodigo");
-  var resultadoElement2 = document.getElementById("resultadoTotal");
-  resultadoElement.textContent = "Devem ser inspecionadas: "+ resultadoPorCodigo + " Peças por código";
-  resultadoElement2.textContent = "Devem ser inspecionadas: "+ resultadoTotal + " Peças no total." ;
-}
+    var inputPorcentagem = document.getElementById("inputPorcentagem").value;
+    var inputPecas = document.getElementById("inputPecas").value;
+    var inputCodigos = document.getElementById("inputCodigos").value;
+  
+  
+    var per = inputPorcentagem / 100;
+  
+    var pecas = parseInt(inputPecas);
+    var codigos = parseInt(inputCodigos);
+  
+    var resultadoPorCodigo = (pecas * per) / codigos;
+    var resultadoTotal = pecas * per;
+  
+   
+    var resultadoElement = document.getElementById("resultadoPorCodigo");
+    var resultadoElement2 = document.getElementById("resultadoTotal");
+    resultadoElement.textContent = "DEVEM SER INSPECIONADAS: "+ resultadoPorCodigo + " PEÇAS POR CÓDIGO !";
+    resultadoElement2.textContent = "DEVEM SER INSPECIONADAS: "+ resultadoTotal + " PEÇAS NO TOTAL !" ;
+  }
